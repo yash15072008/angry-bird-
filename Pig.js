e6ad2e54660d7daf7b9a17db1935015c880d8d6b
yash15072008 <75427466+yash15072008@users.noreply.github.com>
2021-01-15 19:53:23 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,9 @@
+class Pig extends Baseclass {
+
+    constructor(x,y){
+        
+        super(x,y,65,65);
+        this.image =loadImage("sprites/enemy.png");
+    }
+    
+}
--- sketch.js
+++ sketch.js
@@ -3,7 +3,12 @@ const World = Matter.World;
 const Bodies = Matter.Bodies;
 
 var myEngine, myWorld;
-var box1, box2, ground;
+var box1, box2, ground, bg;
+
+function preload() {
+
+    bg =loadImage("sprites/bg.png");
+}
 
 function setup(){
     createCanvas(1200,600);
@@ -12,17 +17,43 @@ function setup(){
     myWorld = myEngine.world;
 
     ground = new Ground(600,590,1200,20);
+    ground2= new Ground(170,450,300,300);
+    
+    bird =new Bird(100,100);
 
     box1 =new Box(800,550);
     box2 =new Box(1000,550);
+    pig1 =new Pig(900,550);
+    log1 =new Log(900,510,300,PI/2);
     
+    box3 =new Box(800,450);
+    box4 =new Box(1000,450);
+    pig2 =new Pig(900,450);
+    log2 =new Log(900,410,300,PI/2);
+
+    box5 =new Box(900,350);
+    log3 =new Log(800,350,150,PI/7);
+    log4 =new Log(1000,350,150,-PI/7);
+ 
 }
 
 function draw(){
-    background(0);
+    background(bg);
     Engine.update(myEngine);
     
     ground.display();
+    ground2.display();
     box1.display();
     box2.display();
+    pig1.display();
+    bird.display();
+    log1.display();
+    box3.display();
+    box4.display();
+    pig2.display();
+    log2.display();
+    log3.display();
+    log4.display();
+    box5.display();
+
 }
